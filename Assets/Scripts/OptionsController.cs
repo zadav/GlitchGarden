a420fa419d2b300630e12ddfdd3c000fa5971d5d
David ZAOUI <david.zaoui@afnor.org>
2017-05-24 18:32:54 +0200

    maj

--- Assets/Scripts/OptionsController.cs
+++ Assets/Scripts/OptionsController.cs
@@ -15,6 +15,7 @@ public class OptionsController : MonoBehaviour {
 		musicManager = GameObject.FindObjectOfType<MusicManager>();
 		
 		volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+		difficultySlider.value = PlayerPrefsManager.GetDifficulty();
 		Debug.Log(musicManager);
 	}
 	
@@ -25,7 +26,12 @@ public class OptionsController : MonoBehaviour {
 	
 	public void SaveAndExit(){
 		PlayerPrefsManager.SetMasterVolume(volumeSlider.value);
+		PlayerPrefsManager.SetDifficulty(difficultySlider.value);
 		levelManager.LoadLevel("01a_Start");
 	}
 	
+	public void SetDefaults(){
+		volumeSlider.value = 0.3f;
+		difficultySlider.value = 1f; 
+	}
 }
